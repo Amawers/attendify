cc81b192c52c63538b1c9294695d2544c0eca031
shadowww2802 <cumpio.justinejay28@gmail.com>
2023-10-12 16:08:28 +0800

    create manage account

---
+++ lib/pages/manage_account.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ManageAccount extends StatelessWidget {
+  const ManageAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
