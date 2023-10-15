248498321055cf7b4b9368c4321e8c063366959a
Amawers <dacoroon.christianjericho@gmail.com>
2023-10-15 10:04:34 +0800

    initial page design okay draft

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,67 +10,109 @@ class HomePage extends StatelessWidget {
       backgroundColor: const Color.fromARGB(255, 8, 22, 49),
       body: Center(
         child: Card(
-          color: const Color.fromARGB(255, 8, 22, 49),
-          child: Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/ATtendify.png', width: 400),
-                Container(
-                  width: 300,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginScreen()),
-                      );
-                    },
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18),
+            color: const Color.fromARGB(255, 8, 22, 49),
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/logo.png', width: 350),
+                  const SizedBox(
+                      height: 16), // Add some space above the buttons
+                  Container(
+                    width: 250,
+                    height: 45,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LoginScreen()),
+                        );
+                      },
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
                         ),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            const Color.fromARGB(255, 255, 255, 255)),
                       ),
-                      backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 255, 255, 255)), // Set your desired background color here
-                    ),
-                    child: const Text(
-                      'Log In',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: const Color(0xFF081631)),
+                      child: const Text(
+                        'Log In',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Color(0xFF081631)),
+                      ),
                     ),
                   ),
-                ),
-
-                const SizedBox(
-                    height: 16), // Add some space between the buttons
-                Container(
-                  width: 300,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Add register functionality here
-                    },
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18),
+                  const SizedBox(height: 16),
+                  // Add some space between the buttons
+                  const SizedBox(
+                    width: 260,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            thickness: 2,
+                          ),
                         ),
-                      ),
-                      backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 255, 255, 255)),
-                    ),
-                    child: const Text(
-                      'Register',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: const Color(0xFF081631)),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Text(
+                            'or',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 255, 255, 255)),
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            thickness: 2,
+                            height: 2,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-        ),
+                  const SizedBox(
+                      height:
+                          16), // Add some space between the line and the "Register" button
+                  Container(
+                    width: 250,
+                    height: 45,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Add register functionality here
+                      },
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                        ),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Color.fromARGB(255, 255, 255, 255)),
+                      ),
+                      child: const Text(
+                        'Register',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: const Color(0xFF081631)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )),
       ),
     );
   }
