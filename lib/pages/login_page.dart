75d7505916927647c12c211b79a678ada9c69098
linds808 <juarez.lindy2@gmail.com>
2023-10-20 00:18:12 +0800

    FIX HEADER,PLACEHOLDER OF EMAIL,PASS AND COLORS

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -13,93 +13,102 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   bool isRememberMe = false;
 
-// kani na part is para sa email
+// EMAIL SECTION
 
   Widget buildEmail() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         const Text(
-          'Email',
+          'Email', //EMAIL HEADER
           style: TextStyle(
-              color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+              color: Color(0xffFFFFFF),
+              fontSize: 16,
+              fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 10),
         Container(
           alignment: Alignment.centerLeft,
           decoration: BoxDecoration(
-              color: Colors.white,
+              color: const Color(0xffFFFFFF),
               borderRadius: BorderRadius.circular(10),
               boxShadow: const [
                 BoxShadow(
-                    color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
+                    color: Color(0xff101010),
+                    blurRadius: 6,
+                    offset: Offset(0, 2))
               ]),
           height: 60,
           child: const TextField(
             keyboardType: TextInputType.emailAddress,
-            style: TextStyle(color: Colors.black87),
+            style: TextStyle(color: Color(0xff101010)), // FIXED THE COLORS
             decoration: InputDecoration(
                 border: InputBorder.none,
                 contentPadding: EdgeInsets.only(top: 14),
                 prefixIcon: Icon(Icons.email, color: Color(0xff8c34eb)),
-                hintText: 'Email',
-                hintStyle: TextStyle(color: Colors.black38)),
+                hintText: 'Ex. JuanDecario@gmail.com', // EMAIL PLACE HOLDER
+                hintStyle: TextStyle(color: Color(0xffD3D3D3))),
           ),
         )
       ],
     );
   }
 
-//kani na part para ni password
+//INPUT PASSWORD
 
   Widget buildPassword() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         const Text(
-          'Password',
+          'Password', //HEADER PASSWORD
           style: TextStyle(
-              color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+              color: Color(0xffFFFFFF),
+              fontSize: 16,
+              fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 10),
         Container(
           alignment: Alignment.centerLeft,
           decoration: BoxDecoration(
-              color: Colors.white,
+              color: const Color(0xffFFFFFF),
               borderRadius: BorderRadius.circular(10),
               boxShadow: const [
                 BoxShadow(
-                    color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
+                    color: Color(0xff101010),
+                    blurRadius: 6,
+                    offset: Offset(0, 2))
               ]),
           height: 60,
           child: const TextField(
             obscureText: true,
-            style: TextStyle(color: Colors.black87),
+            style: TextStyle(color: Color(0xff101010)),
             decoration: InputDecoration(
                 border: InputBorder.none,
                 contentPadding: EdgeInsets.only(top: 14),
                 prefixIcon: Icon(Icons.lock, color: Color(0xff8c34eb)),
-                hintText: 'Password',
-                hintStyle: TextStyle(color: Colors.black38)),
+                hintText: 'Ex.123****', // PLACEHOLDER PASSWORD
+                hintStyle: TextStyle(color: Color(0xffD3D3D3))),
           ),
         )
       ],
     );
   }
 
-// katong si forgot password na
+// FORGOT PASSWORD
 
   Widget buildForgotPassBtn() {
     return Container(
       alignment: Alignment.centerRight,
       child: Padding(
-        padding: EdgeInsets.only(right: 0),
+        padding: const EdgeInsets.only(right: 0),
         child: TextButton(
+          // ignore: avoid_print
           onPressed: () => print("Forgot Password Pressed"),
           child: const Text(
             'Forgot Password?',
             style: TextStyle(
-              color: Colors.white,
+              color: Color(0xffFFFFFF),
               fontWeight: FontWeight.bold,
             ),
           ),
@@ -108,17 +117,18 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
+  // REMEMBER PASSWORD
   Widget buildRememberCb() {
-    return Container(
+    return SizedBox(
       height: 20,
       child: Row(
         children: <Widget>[
           Theme(
-            data: ThemeData(unselectedWidgetColor: Colors.white),
+            data: ThemeData(unselectedWidgetColor: const Color(0xffFFFFFF)),
             child: Checkbox(
               value: isRememberMe,
-              checkColor: Colors.green,
-              activeColor: Colors.white,
+              checkColor: const Color(0xffafc9a4), //CHECK MARK COLOR
+              activeColor: const Color(0xffFFFFFF), // BOX OF THE CHECK MARK
               onChanged: (value) {
                 setState(() {
                   isRememberMe = value!;
@@ -128,33 +138,34 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
           const Text(
             'Remember me',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            style: TextStyle(
+                color: Color(0xffFFFFFF), fontWeight: FontWeight.bold),
           )
         ],
       ),
     );
   }
 
+  // LOGIN BUTTON
   Widget buildLoginBtn() {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 25),
       width: double.infinity,
       child: ElevatedButton(
-         onPressed: () {
-        // Add code to navigate to NavBar when the login button is clicked.
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => NavBar(),
-          ),
-        );
-      },
+        onPressed: () {
+          // Add code to navigate to NavBar when the login button is clicked.
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const NavBar(),
+            ),
+          );
+        },
         style: ElevatedButton.styleFrom(
-          elevation: 5,
+          elevation: 5, backgroundColor: const Color(0xffFFFFFF),
           padding: const EdgeInsets.all(15),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(15),
-          ),
-          primary: Colors.white, // Button background color
+          ), // Button background color
         ),
         child: const Text(
           'LOGIN',
@@ -168,21 +179,23 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
+// SIGN UP BUTTON
   Widget buildSignUpBtn() {
     return GestureDetector(
+      // ignore: avoid_print
       onTap: () => print("Sign Up Pressed"),
       child: RichText(
         text: const TextSpan(children: [
           TextSpan(
               text: 'Don\'t have an Account?',
               style: TextStyle(
-                  color: Colors.white,
+                  color: Color(0xffFFFFFF),
                   fontSize: 18,
                   fontWeight: FontWeight.w500)),
           TextSpan(
               text: 'Sign Up',
               style: TextStyle(
-                  color: Colors.white,
+                  color: Color(0xffFFFFFF),
                   fontSize: 18,
                   fontWeight: FontWeight.bold))
         ]),
@@ -190,6 +203,7 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
+  // BODY SA LOG IN
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -203,10 +217,10 @@ class _LoginScreenState extends State<LoginScreen> {
                 width: double.infinity,
                 decoration: const BoxDecoration(
                     gradient: LinearGradient(
-                  begin: Alignment.topCenter, // question mark ka sakin
+                  begin: Alignment.topCenter, // STILL CONFUSE ABOUT THIS
                   end: Alignment.bottomCenter,
                   colors: [
-                    Color(0xffeb4034), // background color nani ehuheu
+                    Color(0xffeb4034), // BACKGROUND COLORS
                     Color(0xffeb4034),
                     Color(0xffeb4034),
                     Color(0xffeb4034),
@@ -220,11 +234,9 @@ class _LoginScreenState extends State<LoginScreen> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       const Text(
-                        'Sign In', // nara si kato btaw si email text box mao ni
-                        // sya ayay
-
+                        'Sign In',
                         style: TextStyle(
-                            color: Colors.white,
+                            color: Color(0xffFFFFFF),
                             fontSize: 40,
                             fontWeight: FontWeight.bold),
                       ),
